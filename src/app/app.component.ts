e232716e77dfdafb4042a5fc8e1e071e1b71bda6
Regina Santos <60253836+Aquaregina09@users.noreply.github.com>
2023-04-18 18:09:30 +0800

    add datalist without style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon-ui';
+  listForNatureOfGoods = ['fresh fish', 'live bees', 'pink dinosaur', 'consolidation'];
 }
