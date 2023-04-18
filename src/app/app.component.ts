e56b6449331dd2c87925fa0450d83bbbfa574df3
Cyrus <cyrus080600@gmail.com>
2023-04-18 18:43:24 +0800

    Fixed dropdown suggestion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon-ui';
-  listForNatureOfGoods = ['fresh fish', 'live bees', 'pink dinosaur', 'consolidation'];
+  searchTerm: string = "";
+  selectedOption: string = "";
+  listForNatureOfGoods: string[] = ['fresh fish', 'live bees', 'pink dinosaur', 'consolidation'];
+  filteredOptions: string[] = [];
+
+  onSearch() {
+    if (this.searchTerm) {
+      this.filteredOptions = this.listForNatureOfGoods.filter(option => option.toLowerCase().includes(this.searchTerm.toLowerCase()));
+    } else {
+      this.filteredOptions = [];
+    }
+  }
+
+  onOptionSelected(option: string) {
+    this.searchTerm = option;
+    this.filteredOptions = [];
+  }
 }
+  
+
