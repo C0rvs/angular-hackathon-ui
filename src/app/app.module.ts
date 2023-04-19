172196c9a8d1034078b3c360cfb1b466f4a4b9d4
Cyrus <cyrus080600@gmail.com>
2023-04-19 13:05:24 +0800

    Converted autocomplete js to ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field'; // <-- import this module
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule // <-- add this module to the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
